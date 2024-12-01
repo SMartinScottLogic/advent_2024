59cbdee6475d2c27461f2dcd62820994150e4781
Simon Martin <smartin@scottlogic.com>
2024-12-01 05:36:01 +0000

    Part 1 complete

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -4,8 +4,19 @@ use tracing::{debug, event_enabled, info, Level};
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    left: Vec<u64>,
+    right: Vec<u64>,
+}
+impl Solution {
+    fn distance(a: &ResultType, b: &ResultType) -> ResultType {
+        if a < b {
+            b - a
+        } else {
+            a - b
+        }
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -14,7 +25,11 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
     fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
-            // Implement for problem
+            let mut i = line.split_whitespace().map(|s| s.parse::<u64>().unwrap());
+            let left = i.next().unwrap();
+            let right = i.next().unwrap();
+            solution.left.push(left);
+            solution.right.push(right);
         }
         Ok(solution)
     }
@@ -24,8 +39,16 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let mut left = self.left.clone();
+        left.sort();
+        let mut right = self.right.clone();
+        right.sort();
+        for (a, b) in left.iter().zip(right.iter()) {
+            info!(a, b, "sorted");
+        }
+        let answer = left.iter().zip(right.iter()).map(|(a, b)| Self::distance(a, b)).sum();
         // Implement for problem
-        Ok(0)
+        Ok(answer)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
