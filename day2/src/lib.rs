a1dae5b81920b768a46c365d928b973454b7b8d3
Simon Martin <smartin@scottlogic.com>
2024-12-02 05:14:55 +0000

    Day2 part 1 complete

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,12 +1,19 @@
-use std::io::{BufRead, BufReader};
+use std::{cmp::Ordering, io::{BufRead, BufReader}};
+use itertools::Itertools;
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
 
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    reports: Vec<Vec<ResultType>>,
+}
+impl Solution {
+    fn add_report(&mut self, levels: Vec<ResultType>) {
+        self.reports.push(levels);
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +23,8 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            let levels = line.split_whitespace().flat_map(|v| v.parse()).collect();
+            solution.add_report(levels);
         }
         Ok(solution)
     }
@@ -26,7 +35,8 @@ impl utils::Solution for Solution {
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
         // Implement for problem
-        Ok(0)
+        let answer = self.reports.iter().filter(|v| Self::is_safe(*v)).count();
+        Ok(answer as ResultType)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
@@ -34,7 +44,42 @@ impl utils::Solution for Solution {
         Ok(0)
     }
 }
-
+impl Solution {
+    fn is_safe(report: &Vec<ResultType>) -> bool {
+        let mut dir = Ordering::Equal;
+        let mut last = 0;
+        for (i, cur) in report.iter().enumerate() {
+            if i==0 {
+                last = *cur;
+                continue;
+            }
+            if last == *cur {
+                return false;
+            }
+            if last > *cur {
+                if dir == Ordering::Less {
+                    return false;
+                }
+                if last - *cur > 3 {
+                    return false;
+                }
+                dir = Ordering::Greater;
+            }
+            if last < *cur {
+                if dir == Ordering::Greater {
+                    return false;
+                }
+                if *cur - last > 3 {
+                    return false;
+                }
+                dir = Ordering::Less;
+            }
+            last = *cur;
+        }
+        info!(report = debug(report), "safe");
+        true
+    }
+}
 #[cfg(test)]
 mod test {
     use super::*;
