27ab3e5af89aab7ab29757589009af026efa7f14
Simon Martin <smartin@scottlogic.com>
2024-12-08 05:40:55 +0000

    Day 8 complete

--- day8/src/lib.rs
+++ day8/src/lib.rs
@@ -1,12 +1,22 @@
-use std::io::{BufRead, BufReader};
+use std::{
+    collections::HashSet,
+    io::{BufRead, BufReader},
+};
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
+use utils::{Point, SparseGrid};
 
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    grid: SparseGrid<char, isize>,
+}
+impl Solution {
+    fn add_antenna(&mut self, x: usize, y: usize, c: char) {
+        self.grid.set(&Point::new(x as isize, y as isize), c);
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -14,8 +24,11 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
 
     fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
         let mut solution = Self::default();
-        for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
+        for (y, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            for (x, c) in line.chars().enumerate() {
+                solution.add_antenna(x, y, c);
+            }
         }
         Ok(solution)
     }
@@ -25,30 +38,106 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
-        // Implement for problem
-        Ok(0)
+        let mut antinodes = HashSet::new();
+        for (p1, c1) in self.grid.iter() {
+            for (p2, c2) in self.grid.iter() {
+                if p1 == p2 || c1 != c2 {
+                    continue;
+                }
+                if *c1 == '.' {
+                    continue;
+                }
+                debug!(?p1, ?p2, ?c1, ?c2);
+
+                let dx = p1.x() - p2.x();
+                let dy = p1.y() - p2.y();
+                // antinode 1
+                {
+                    let x = p1.x() + dx;
+                    let y = p1.y() + dy;
+                    let p = Point::new(x, y);
+                    if p != *p1 && p != *p2 && self.grid.contains(&p) {
+                        debug!(?p, ?p1, ?p2, contained = self.grid.contains(&p));
+                        antinodes.insert(p);
+                    }
+                }
+                // antinode 2
+                {
+                    let x = p2.x() + dx;
+                    let y = p2.y() + dy;
+                    let p = Point::new(x, y);
+                    if p != *p1 && p != *p2 && self.grid.contains(&p) {
+                        debug!(?p, ?p1, ?p2, contained = self.grid.contains(&p));
+                        antinodes.insert(p);
+                    }
+                }
+                // antinode 3
+                {
+                    let x = p1.x() - dx;
+                    let y = p1.y() - dy;
+                    let p = Point::new(x, y);
+                    if p != *p1 && p != *p2 && self.grid.contains(&p) {
+                        debug!(?p, ?p1, ?p2, contained = self.grid.contains(&p));
+                        antinodes.insert(p);
+                    }
+                }
+                // antinode 4
+                {
+                    let x = p2.x() - dx;
+                    let y = p2.y() - dy;
+                    let p = Point::new(x, y);
+                    if p != *p1 && p != *p2 && self.grid.contains(&p) {
+                        debug!(?p, ?p1, ?p2, contained = self.grid.contains(&p));
+                        antinodes.insert(p);
+                    }
+                }
+            }
+        }
+        Ok(antinodes.len() as ResultType)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let mut antinodes = HashSet::new();
+        for (p1, c1) in self.grid.iter() {
+            for (p2, c2) in self.grid.iter() {
+                if p1 == p2 || c1 != c2 {
+                    continue;
+                }
+                if *c1 == '.' {
+                    continue;
+                }
+                debug!(?p1, ?p2, ?c1, ?c2);
+
+                let dx = p1.x() - p2.x();
+                let dy = p1.y() - p2.y();
+                let mut x = p1.x();
+                let mut y = p1.y();
+
+                loop {
+                    let p = Point::new(x, y);
+                    if !self.grid.contains(&p) {
+                        break;
+                    }
+                    antinodes.insert(p);
+                    x -= dx;
+                    y -= dy;
+                }
+
+                let mut x = p1.x();
+                let mut y = p1.y();
+
+                loop {
+                    let p = Point::new(x, y);
+                    if !self.grid.contains(&p) {
+                        break;
+                    }
+                    antinodes.insert(p);
+                    x += dx;
+                    y += dy;
+                }
+            }
+        }
         // Implement for problem
-        Ok(0)
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::io::BufReader;
-
-    use tracing_test::traced_test;
-    use utils::Solution;
-
-    #[test]
-    #[traced_test]
-    fn read() {
-        let input = "replace for problem";
-        let r = BufReader::new(input.as_bytes());
-        let s = crate::Solution::try_from(r).unwrap();
-        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
+        Ok(antinodes.len() as ResultType)
     }
 }
