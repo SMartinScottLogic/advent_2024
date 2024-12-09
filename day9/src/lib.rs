b7f1f675886e53901632f3473c4507f872cd8fb5
Simon Martin <smartin@scottlogic.com>
2024-12-09 06:22:02 +0000

    Day 9 part 1 complete

--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -5,8 +5,14 @@ use tracing::{debug, event_enabled, info, Level};
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    diskmap: Vec<ResultType>,
+}
+impl Solution {
+    fn set_diskmap(&mut self, map: Vec<ResultType>) {
+        self.diskmap = map;
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -15,7 +21,12 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
     fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
+            let diskmap = line
+                .chars()
+                .map(|c| format!("{}", c).parse().unwrap())
+                .collect();
             // Implement for problem
+            solution.set_diskmap(diskmap);
         }
         Ok(solution)
     }
@@ -26,15 +37,87 @@ impl utils::Solution for Solution {
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
         // Implement for problem
-        Ok(0)
+        let mut blocks = diskmap(&self.diskmap);
+        let mut s = 0;
+        let mut e = blocks.len() - 1;
+
+        loop {
+            while blocks[s] != Block::Empty {
+                s += 1;
+            }
+            while blocks[e] == Block::Empty {
+                e -= 1;
+            }
+            if s >= e {
+                break;
+            }
+            blocks[s] = blocks[e].clone();
+            blocks[e] = Block::Empty;
+        }
+        debug!(?blocks, "blocks");
+
+        Ok(checksum(&blocks))
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
         // Implement for problem
-        Ok(0)
+        let mut blocks = diskmap(&self.diskmap);
+
+        loop {
+            let mut s = 0;
+            let mut e = blocks.len() - 1;
+
+            while blocks[e] == Block::Empty {
+                e -= 1;
+            }
+
+            blocks[s] = blocks[e].clone();
+            blocks[e] = Block::Empty;
+            if s == 0 {
+                break;
+            }
+        }
+        debug!(?blocks, "blocks");
+
+        Ok(checksum(&blocks))
     }
 }
 
+fn diskmap(map: &[ResultType]) -> Vec<Block> {
+    let mut blocks = Vec::new();
+    for (id, c) in map.iter().enumerate() {
+        let s = if id % 2 == 0 {
+            Block::FileBlock((id / 2).try_into().unwrap())
+        } else {
+            Block::Empty
+        };
+        for _ in 0..*c {
+            blocks.push(s.clone());
+        }
+    }
+    debug!(?blocks, "blocks");
+    blocks
+}
+
+fn checksum(blocks: &[Block]) -> ResultType {
+    blocks
+        .iter()
+        .enumerate()
+        .map(|(pos, block)| {
+            if let Block::FileBlock(id) = block {
+                id * (pos as ResultType)
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Block {
+    Empty,
+    FileBlock(ResultType),
+}
 #[cfg(test)]
 mod test {
     use super::*;
