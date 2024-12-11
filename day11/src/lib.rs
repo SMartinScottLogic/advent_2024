9200241446198d189f93995544d541bb689d9a81
Simon Martin <havvoric@gmail.com>
2024-12-11 05:24:55 +0000

    Day 11 part 1 complete

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -5,8 +5,14 @@ use tracing::{debug, event_enabled, info, Level};
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    stones: Vec<ResultType>,
+}
+impl Solution {
+    fn set_stones(&mut self, stones: Vec<ResultType>) {
+        self.stones = stones;
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +22,11 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            let input = line
+                .split_whitespace()
+                .map(|v| v.parse().unwrap())
+                .collect();
+            solution.set_stones(input);
         }
         Ok(solution)
     }
@@ -25,14 +36,62 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let mut stones = self.stones.clone();
+        info!(i = 0, ?stones);
+        for i in 0..25 {
+            let mut new_stones = Vec::new();
+            for stone in stones {
+                if stone == 0 {
+                    new_stones.push(1);
+                } else if has_even_digits(stone) {
+                    let (l, r) = split(stone);
+                    new_stones.push(l);
+                    new_stones.push(r);
+                } else {
+                    new_stones.push(stone * 2024);
+                }
+            }
+            stones = new_stones;
+            info!(i = i + 1, ?stones);
+        }
         // Implement for problem
-        Ok(0)
+        Ok(stones.len() as ResultType)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let mut stones = self.stones.clone();
+        debug!(i = 0, ?stones);
+        for i in 0..10 {
+            let mut new_stones = Vec::new();
+            for stone in stones {
+                if stone == 0 {
+                    new_stones.push(1);
+                } else if has_even_digits(stone) {
+                    let (l, r) = split(stone);
+                    new_stones.push(l);
+                    new_stones.push(r);
+                } else {
+                    new_stones.push(stone * 2024);
+                }
+            }
+            stones = new_stones;
+            info!(i = i + 1, ?stones);
+            debug!(i = i + 1, num_stones = stones.len());
+        }
         // Implement for problem
-        Ok(0)
+        Ok(stones.len() as ResultType)
     }
+}
+
+fn has_even_digits(v: ResultType) -> bool {
+    format!("{}", v).len() % 2 == 0
+}
+
+fn split(v: ResultType) -> (ResultType, ResultType) {
+    let i = format!("{}", v);
+    let s = i.len() / 2;
+    let (a, b) = i.split_at(s);
+    (a.to_owned().parse().unwrap(), b.to_owned().parse().unwrap())
 }
 
 #[cfg(test)]
