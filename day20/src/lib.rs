8d4d91a5df70900b92242ce9c664c1a554cdd1c6
Simon Martin <havvoric@gmail.com>
2024-12-20 06:58:35 +0000

    Day 20 part 2 complete

--- day20/src/lib.rs
+++ day20/src/lib.rs
@@ -4,7 +4,7 @@ use std::{
 };
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
-use utils::{grid::Matrix, point::Point};
+use utils::grid::Matrix;
 
 pub type ResultType = u64;
 
@@ -55,7 +55,7 @@ impl utils::Solution for Solution {
         // Get distances along best path
         let d = self.distance(start.0, start.1, end.0, end.1);
         let max = d.get(&(end.0, end.1)).unwrap();
-        info!(max, ?d);
+        debug!(max, ?d);
 
         let mut r = 0;
         for y in 0..=self.grid.max_y() {
@@ -97,8 +97,59 @@ impl utils::Solution for Solution {
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
-        // Implement for problem
-        Ok(0)
+        // Find start and end
+        let mut start = (0, 0, false);
+        let mut end = (0, 0);
+        for y in 0..=self.grid.max_y() {
+            for x in 0..=self.grid.max_x() {
+                match self.grid.get(x, y) {
+                    Some('S') => {
+                        start = (x, y, false);
+                    }
+                    Some('E') => end = (x, y),
+                    _ => {}
+                }
+            }
+        }
+        // Get distances along best path
+        let d = self.distance(start.0, start.1, end.0, end.1);
+        let max = d.get(&(end.0, end.1)).unwrap();
+        debug!(max, ?d);
+
+        let mut r = 0;
+        for y in 0..=self.grid.max_y() {
+            for x in 0..=self.grid.max_x() {
+                if let Some(c) = self.grid.get(x, y) {
+                    if *c != '.' && *c != 'S' && *c != 'E' {
+                        continue;
+                    }
+                    if let Some(cost1) = d.get(&(x, y)) {
+                        for dy in -20..=20_isize {
+                            for dx in -20..=20_isize {
+                                let manhattan_distance = (dx.abs() + dy.abs()) as i32;
+                                if manhattan_distance > 20 {
+                                    continue;
+                                }
+                                let nx = x + dx;
+                                let ny = y + dy;
+                                if let Some(c) = self.grid.get(nx, ny) {
+                                    if *c != '.' && *c != 'S' && *c != 'E' {
+                                        continue;
+                                    }
+                                }
+                                if let Some(cost2) = d.get(&(nx, ny)) {
+                                    let saving = cost1 - cost2 - manhattan_distance;
+                                    if saving >= 100 {
+                                        r += 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Ok(r)
     }
 }
 
@@ -140,144 +191,5 @@ impl Solution {
             }
         }
         distances
-    }
-
-    fn count_paths(
-        &self,
-        x: isize,
-        y: isize,
-        end_x: isize,
-        end_y: isize,
-        has_cheated: bool,
-        visited: &HashSet<(isize, isize)>,
-        max: usize,
-        distance: usize,
-        distances: &mut HashMap<usize, usize>,
-    ) {
-        if x == end_x && y == end_y {
-            distances.insert(distance, 1);
-        } else if distance >= max {
-            // Cannot progress - distance to end will exceed non-cheat best
-        } else {
-            let mut visited = visited.clone();
-            visited.insert((x, y));
-            // Normal moves
-            for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
-                let nx = x + dx;
-                let ny = y + dy;
-                match self.grid.get(nx, ny) {
-                    Some('.') | Some('S') | Some('E') => {
-                        if !visited.contains(&(nx, ny)) {
-                            self.count_paths(
-                                nx,
-                                ny,
-                                end_x,
-                                end_y,
-                                has_cheated,
-                                &visited,
-                                max,
-                                1 + distance,
-                                distances,
-                            );
-                        }
-                    }
-                    Some('#') => {
-                        if !has_cheated {
-                            if let Some(c) = self.grid.get(nx + dx, ny + dy) {
-                                if *c == '.' || *c == 'S' || *c == 'E' {
-                                    if !visited.contains(&(nx, ny)) {
-                                        self.count_paths(
-                                            nx + dx,
-                                            ny + dy,
-                                            end_x,
-                                            end_y,
-                                            true,
-                                            &visited,
-                                            max,
-                                            2 + distance,
-                                            distances,
-                                        );
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
-
-    fn best_path(&self, can_cheat: bool) -> usize {
-        // Find start and end
-        let mut start = (0, 0, false);
-        let mut end = (0, 0);
-        for y in 0..=self.grid.max_y() {
-            for x in 0..=self.grid.max_x() {
-                match self.grid.get(x, y) {
-                    Some('S') => {
-                        start = (x, y, true);
-                    }
-                    Some('E') => end = (x, y),
-                    _ => {}
-                }
-            }
-        }
-        let success = |(x, y, _has_cheated): &(isize, isize, bool)| {
-            info!(x, y, ?end, ?start);
-            *x == end.0 && *y == end.1
-        };
-        let heuristic = |s: &(isize, isize, bool)| s.0.abs_diff(end.0) + s.1.abs_diff(end.1);
-        let successors = |(x, y, has_cheated): &(isize, isize, bool)| {
-            let mut next_states = Vec::new();
-            // Normal moves
-            for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
-                let nx = x + dx;
-                let ny = y + dy;
-                match self.grid.get(nx, ny) {
-                    Some('.') => next_states.push(((nx, ny, *has_cheated), 1)),
-                    Some('S') => next_states.push(((nx, ny, *has_cheated), 1)),
-                    Some('E') => next_states.push(((nx, ny, *has_cheated), 1)),
-                    Some('#') => {
-                        if !has_cheated {
-                            if let Some(c) = self.grid.get(nx + dx, ny + dy) {
-                                if *c == '.' || *c == 'S' || *c == 'E' {
-                                    next_states.push(((nx + dx, ny + dy, true), 2));
-                                }
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            }
-            next_states
-        };
-        let r = pathfinding::directed::astar::astar(
-            &start,
-            successors,
-            |s| heuristic(s),
-            |s| success(&s),
-        )
-        .unwrap();
-        // Implement for problem
-        info!(?r);
-        r.1
-    }
-}
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::io::BufReader;
-
-    use tracing_test::traced_test;
-    use utils::Solution;
-
-    #[test]
-    #[traced_test]
-    fn read() {
-        let input = "replace for problem";
-        let r = BufReader::new(input.as_bytes());
-        let s = crate::Solution::try_from(r).unwrap();
-        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
     }
 }
