54b26b4233ad4612a59f668cbd12d1d22c9237c3
Simon Martin <havvoric@gmail.com>
2024-12-22 07:18:39 +0000

    Day 22 complete

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -1,12 +1,22 @@
-use std::io::{BufRead, BufReader};
+use itertools::Itertools;
+use std::{
+    collections::{HashMap, HashSet},
+    io::{BufRead, BufReader},
+};
 #[allow(unused_imports)]
 use tracing::{debug, event_enabled, info, Level};
 
-pub type ResultType = u64;
+pub type ResultType = i64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    initials: Vec<ResultType>,
+}
+impl Solution {
+    fn add_initial(&mut self, initial: ResultType) {
+        self.initials.push(initial);
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +26,8 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            let initial = line.parse().unwrap();
+            solution.add_initial(initial);
         }
         Ok(solution)
     }
@@ -25,30 +37,58 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let mut total = 0;
+        for initial in &self.initials {
+            let mut v = *initial;
+            for _ in 1..=2000 {
+                v = next_secret(v);
+                debug!(?v, initial);
+            }
+            debug!(?v, initial);
+            total += v;
+        }
         // Implement for problem
-        Ok(0)
+        Ok(total)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        let mut bananas: HashMap<(i64, i64, i64, i64), i64> = HashMap::new();
+        for initial in &self.initials {
+            let mut values = Vec::new();
+            let mut v = *initial;
+            for _ in 1..=2000 {
+                v = next_secret(v);
+                values.push(v);
+            }
+            // comvert to prices
+            for v in values.iter_mut() {
+                *v %= 10;
+            }
+            let mut seen = HashSet::new();
+            for (a, b, c, d, e) in values.iter().tuple_windows() {
+                let key = (b - a, c - b, d - c, e - d);
+                if seen.insert(key) {
+                    *bananas.entry(key).or_default() += *e;
+                }
+            }
+        }
+        let max = bananas.values().max().unwrap();
         // Implement for problem
-        Ok(0)
+        Ok(*max)
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::io::BufReader;
+fn next_secret(initial: i64) -> i64 {
+    let mut i = prune(mix(initial, initial * 64));
+    i = prune(mix(i, i / 32));
+    i = prune(mix(i, i * 2048));
+    i
+}
 
-    use tracing_test::traced_test;
-    use utils::Solution;
+fn mix(i: i64, v: i64) -> i64 {
+    v ^ i
+}
 
-    #[test]
-    #[traced_test]
-    fn read() {
-        let input = "replace for problem";
-        let r = BufReader::new(input.as_bytes());
-        let s = crate::Solution::try_from(r).unwrap();
-        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
-    }
+fn prune(v: i64) -> i64 {
+    v % 16777216
 }
